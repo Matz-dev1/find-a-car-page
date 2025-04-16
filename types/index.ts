9100acace7c10887f68d11b63682afdc0d456797
Matz-dev1 <mzapolski0@gmail.com>
2025-04-16 17:58:00 +0200

    commit

---
+++ types/index.ts
@@ -0,0 +1,34 @@
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+  title: string;
+  containerStyles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit";
+  textStyles?: string;
+  rightIcon?: string;
+  isDisabled?: boolean;
+}
+
+export interface SearchManufacturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+  class: string;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+}
+export interface FilterProps {
+  manufacturer: string;
+  year: number;
+  fuel: string;
+  model: string;
+}
---
+++ utils/index.ts
@@ -0,0 +1,61 @@
+import { manufacturers } from "@/constants";
+import { CarProps, FilterProps } from "@/types";
+
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, fuel } = filters;
+  const headers = {
+    "x-rapidapi-key": "95a7a825ecmshbf7a0c34bc8af05p1ec46djsnccf9420b2c09",
+    "x-rapidapi-host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+  const response = await fetch(
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&fuel_type=${fuel}`,
+    { headers: headers }
+  );
+  const result = await response.json();
+  return result;
+}
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+  const { make, model, year } = car;
+
+  url.searchParams.append(
+    "customer",
+    process.env.NEXT_PUBLIC_IMAGIN_API_KEY || ""
+  );
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  // url.searchParams.append('zoomLevel', zoomLevel);
+  url.searchParams.append("angle", `${angle}`);
+
+  return `${url}`;
+};
+
+export const updateSearchParams = (type: string, value: string) => {
+  // Get the current URL search params
+  const searchParams = new URLSearchParams(window.location.search);
+
+  // Set the specified search parameter to the given value
+  searchParams.set(type, value);
+
+  const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+
+  return newPathname;
+};
+
+export const deleteSearchParams = (type: string) => {
+  // Set the specified search parameter to the given value
+  const newSearchParams = new URLSearchParams(window.location.search);
+
+  // Delete the specified search parameter
+  newSearchParams.delete(type.toLocaleLowerCase());
+
+  // Construct the updated URL pathname with the deleted search parameter
+  const newPathname = `${
+    window.location.pathname
+  }?${newSearchParams.toString()}`;
+
+  return newPathname;
+};
